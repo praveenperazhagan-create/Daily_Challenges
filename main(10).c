4bdbd0ce4fb9571e36d5baddba91890a998fe05d
praveenperazhagan-create <praveenperazhagan@gmail.com>
2025-10-26 22:42:08 +0530

    Add files via upload

---
+++ main(10).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   char ch;
+   float de;
+   double dde;
+    printf("%d",sizeof(a));
+    printf("%d",sizeof(ch));
+    printf("%d",sizeof(de));
+    
+    printf("%d",sizeof(dde));
+   
+    return 0;
+}
